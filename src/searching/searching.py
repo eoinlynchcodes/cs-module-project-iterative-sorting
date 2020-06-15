0c273db21420bd5b279604e6f80c6575341112ac
eoinlynchcodes <eoin95lynch@gmail.com>
2020-06-15 15:05:09 +0100

    All tests pass.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,14 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    low, high = 0, len(arr) -1
+    while low <= high:
+        middle = (low + high)
+        if arr[middle] < target:
+            low = middle + 1
+        elif target < arr[middle]:
+            high = middle - 1
+        else:
+            return middle
 
     return -1  # not found
